fea9a60702edc248a52344593dd6d48e647e0ab2
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-29 18:19:08 +0200

    Update client.py
    Already pull the repo in bash.

--- client/python/client.py
+++ client/python/client.py
@@ -46,7 +46,7 @@ if True:
 	os.system('sudo cp /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime')
 	writelog('time zone')
 	f = open("/home/pi/config.txt", 'w')
-	sstr = '{"reverse_ssh_server": "31.20.182.94", "version":"00000"}'
+	sstr = '{"reverse_ssh_server": "henk.waaromzomoeilijk.nl", "version":"00000"}'
 	f.write(sstr)
 	f.close()
 	writelog('config.txt')
@@ -54,7 +54,7 @@ if True:
 	f.write('12345')
 	f.close()
 	f = open("/home/pi/ipaddress", 'w')
-	f.write('31.20.182.94')
+	f.write('henk.waaromzomoeilijk.nl')
 	f.close()
 	f = open("/bin/l", 'w')
 	f.write('ls -l $1')
@@ -62,14 +62,7 @@ if True:
 	os.system('sudo chmod 777 /bin/l')
 	writelog('/home/pi/ipaddress')
 	sstr = "* * * * * sudo python3 /home/pi/m.py >/dev/null 2>&1 &"
-	sstr += "\n" + '*/5 * * * * sshpass -p roma2- ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/pi/ssh_port"):localhost:22 pi@$(cat \"/home/pi/ipaddress\") -p 9212'
+	sstr += "\n" + '*/5 * * * * ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/pi/ssh_port"):localhost:80 remote@$(cat \"/home/pi/ipaddress\") -p 9212'
 	sstr = "cat <(crontab -l) <(echo '"+sstr+"') | crontab -"
 	subprocess.call(['bash', '-c', sstr])
 	writelog('crontab')
-
-	ossystem('cd /home/pi/ && git clone "https://github.com/timmeh1955/aaa.git"')
-	files = ['a.py','ffunctions.py','m.py']
-	for ffile in files:
-		ossystem('sudo mv /home/pi/aaa/' + ffile + ' /home/pi')
-	ossystem('sudo mv aaa /dev/shm')
-	ossystem('sudo rm /home/pi/client.py')
