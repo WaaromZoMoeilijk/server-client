3391be31f0247ee0b77f8629113fed06e05c3c57
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-29 20:14:32 +0200

    Update lib.sh

--- client/lib.sh
+++ client/lib.sh
@@ -3,7 +3,8 @@
 CONFIG="/boot/config.txt"
 GITDIR="/var/opt/wzm"
 REPO="https://github.com/ezraholm50/server-client"  
-DJANGO="/home/pi"
+DJANGO="/home/pi/pidjango"
+TEMPPI="/home/pi"
 
 WANIP4=$(curl -s -k -m 5 https://ipv4bot.whatismyipaddress.com)
 GATEWAY=$(ip route | grep default | awk '{print $3}')
