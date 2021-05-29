3fdf9ab0d9974f2e99d4901fcb196e9c620efdd6
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-29 20:12:39 +0200

    Update get-sshkey-client.sh

--- server/scripts/python-functions/get-sshkey-client.sh
+++ server/scripts/python-functions/get-sshkey-client.sh
@@ -4,5 +4,6 @@
 echo "" >> /home/remote/.ssh/authorized_keys                      # (source on client & destination on server)
 echo "# Username: $(cat /etc/hostname)" >> /home/remote/.ssh/authorized_keys # (source on client & destination on server)
 cat /home/pi/.ssh/id_rsa.pub >> /home/remote/.ssh/authorized_keys # (source on client & destination on server)
+ssh-copy-id -i /home/pi/.ssh/id_rsa.pub remote@henk.waaromzomoeilijk.nl -p 9212
 
 exit 0
