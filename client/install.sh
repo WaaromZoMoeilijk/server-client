36e658ebed8a771c85171e6d25a78249e96c38a1
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-29 10:47:48 +0200

    Update install.sh

--- client/install.sh
+++ client/install.sh
@@ -47,40 +47,41 @@ fi
 if [ -f "/etc/rc.local" ]; then
       echo "RC.local exists"
 else  
-      cat > /etc/systemd/system/rc-local.service <<EOF
-      [Unit]
-      Description=/etc/rc.local
-      ConditionPathExists=/etc/rc.local
 
-      [Service]
-      Type=forking
-      ExecStart=/etc/rc.local start
-      TimeoutSec=0
-      StandardOutput=tty
-      RemainAfterExit=yes
-      SysVStartPriority=99
+cat > /etc/systemd/system/rc-local.service <<EOF
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
 
-      [Install]
-      WantedBy=multi-user.target
-      EOF
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
 
-      # Add rc.local file
-      cat > /etc/rc.local <<EOF
-      #!/bin/sh -e
-      #
-      # rc.local
-      #
-      # This script is executed at the end of each multiuser runlevel.
-      # Make sure that the script will "exit 0" on success or any other
-      # value on error.
-      #
-      # In order to enable or disable this script just change the execution
-      # bits.
-      #
-      # By default this script does nothing.
+[Install]
+WantedBy=multi-user.target
+EOF
 
-      exit 0
-      EOF
+# Add rc.local file
+cat > /etc/rc.local <<EOF
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+exit 0
+EOF
 
       # Set execute permission
       chmod +x /etc/rc.local
