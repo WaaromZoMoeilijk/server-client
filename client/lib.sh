4ddb40a463488663fbfd524dad2a436f67804dca
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-29 20:57:58 +0200

    Update lib.sh

--- client/lib.sh
+++ client/lib.sh
@@ -9,7 +9,7 @@ TEMPPI="/home/pi"
 #WANIP4=$(curl -s -k -m 5 https://ipv4bot.whatismyipaddress.com)
 #GATEWAY=$(ip route | grep default | awk '{print $3}')
 #IFACE=$(ip r | grep "default via" | awk '{print $5}')
-ADDRESS=$(hostname -I | cut -d ' ' -f 1)
+#ADDRESS=$(hostname -I | cut -d ' ' -f 1)
 
 #########################
 '''
