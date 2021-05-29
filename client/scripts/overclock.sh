e7d555024026ef7f12c6c9bbf356c92da85a3dcd
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-29 18:01:31 +0200

    Update overclock.sh

--- client/scripts/overclock.sh
+++ client/scripts/overclock.sh
@@ -3,9 +3,6 @@
 # https://www.raspberrypi.org/documentation/configuration/config-txt/overclocking.md
 # https://elinux.org/RPiconfig
 
-# Variable
-CONFIG="/boot/config.txt"
-
 sed -i '/arm_freq=/d' "$CONFIG"
 sed -i '/arm_freq_min=/d' "$CONFIG"
 sed -i '/over_voltage=/d' "$CONFIG"
