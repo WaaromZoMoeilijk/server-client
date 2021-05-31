e249529ccf7ecd50da7b2971cbe5f88c84483434
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-31 14:00:34 +0200

    Update client.py

--- client/python/client.py
+++ client/python/client.py
@@ -52,7 +52,11 @@ if True:
 	os.system('sudo chmod 777 /bin/l')
 	writelog('/home/pi/ipaddress')
 	sstr = "* * * * * sudo python3 /home/pi/m.py >/dev/null 2>&1 &"
-	sstr += "\n" + '*/5 * * * * ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/pi/ssh_port"):localhost:80 remote@$(cat \"/home/pi/ipaddress\") -p 9212'
+	sstr += "\n" + '*/5 * * * * sshpass -p roma2- ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/pi/ssh_port"):localhost:22 pi@$(cat \"/home/pi/ipaddress\") -p 9212'	
+	#sstr += "\n" + '*/5 * * * * ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/pi/ssh_port"):localhost:80 remote@$(cat \"/home/pi/ipaddress\") -p 9212'
 	sstr = "cat <(crontab -l) <(echo '"+sstr+"') | crontab -"
 	subprocess.call(['bash', '-c', sstr])
 	writelog('crontab')
+	
+
+
