2ad62af9e51333ef8ddf1c4751d8f2ad8a8fde00
Ezra <ezra@waaromzomoeilijk.nl>
2021-05-31 15:22:13 +0200

    Update install.sh

---
+++ server/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# info@waaromzomoeilijk.nl
+
+###################################
+# Variables & functions
+source <(curl -sL https://raw.githubusercontent.com/ezraholm50/server-client/main/client/lib.sh)
