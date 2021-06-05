15c48acb796717f28f6ce3955de80f0da3e1b69b
Ezra <ezra@waaromzomoeilijk.nl>
2021-06-05 22:53:12 +0200

    Update lib.sh

--- client/lib.sh
+++ client/lib.sh
@@ -161,3 +161,137 @@ cleanup_open_port() {
         apt autoremove -y
     fi
 }
+
+# Install_if_not program
+install_if_not() {
+if ! dpkg-query -W -f='${Status}' "${1}" | grep -q "ok installed"
+then
+    apt update -q4 & spinner_loading && RUNLEVEL=1 apt install "${1}" -y
+fi
+}
+
+# Check if program is installed (is_this_installed apache2)
+is_this_installed() {
+if dpkg-query -W -f='${Status}' "${1}" | grep -q "ok installed"
+then
+    return 0
+else
+    return 1
+fi
+}
+
+# Check if program is installed (stop_if_installed apache2)
+stop_if_installed() {
+if [ "$(dpkg-query -W -f='${Status}' "${1}" 2>/dev/null | grep -c "ok installed")" == "1" ]
+then
+    print_text_in_color "$IRed" "${1} is installed, it must be a clean server."
+    exit 1
+fi
+}
+
+# Check if port is open # check_open_port 443 domain.example.com
+check_open_port() {
+print_text_in_color "$ICyan" "Checking if port ${1} is open with https://www.networkappers.com/tools/open-port-checker..."
+install_if_not curl
+# WAN Address
+if check_command curl -s -H 'Cache-Control: no-cache' -H 'Referer: https://www.networkappers.com/tools/open-port-checker' "https://networkappers.com/api/port.php?ip=${2}&port=${1}" | grep -q "open"
+then
+    print_text_in_color "$IGreen" "Port ${1} is open on ${2}!"
+# Domain name
+elif check_command curl -s -H 'Cache-Control: no-cache' -H 'Referer: https://www.networkappers.com/tools/open-port-checker' "https://www.networkappers.com/api/port.php?ip=${2}&port=${1}" | grep -q "open"
+then
+    print_text_in_color "$IGreen" "Port ${1} is open on ${2}!"
+else
+    msg_box "It seems like the port ${1} is closed. This could happened when your
+ISP has blocked the port, or the port isn't open.
+If you are 100% sure the port ${1} is open, you can choose to
+continue. There are no guarantees that it will work though,
+since the service depends on port ${1} being open and
+accessible from outside your network."
+    if ! yesno_box_no "Are you 100% sure the port ${1} is open?"
+    then
+        msg_box "Port $1 is not open on either ${WANIP4} or ${2}."
+        any_key "Press any key to exit..."
+        exit 1
+    fi
+fi
+}
+
+# Print text in color
+print_text_in_color() {
+printf "%b%s%b\n" "$1" "$2" "$Color_Off"
+}
+
+## bash colors
+# Reset
+Color_Off='\e[0m'       # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+# Underline
+UBlack='\e[4;30m'       # Black
+URed='\e[4;31m'         # Red
+UGreen='\e[4;32m'       # Green
+UYellow='\e[4;33m'      # Yellow
+UBlue='\e[4;34m'        # Blue
+UPurple='\e[4;35m'      # Purple
+UCyan='\e[4;36m'        # Cyan
+UWhite='\e[4;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+# High Intensity
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\e[1;90m'      # Black
+BIRed='\e[1;91m'        # Red
+BIGreen='\e[1;92m'      # Green
+BIYellow='\e[1;93m'     # Yellow
+BIBlue='\e[1;94m'       # Blue
+BIPurple='\e[1;95m'     # Purple
+BICyan='\e[1;96m'       # Cyan
+BIWhite='\e[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\e[0;100m'   # Black
+On_IRed='\e[0;101m'     # Red
+On_IGreen='\e[0;102m'   # Green
+On_IYellow='\e[0;103m'  # Yellow
+On_IBlue='\e[0;104m'    # Blue
+On_IPurple='\e[0;105m'  # Purple
+On_ICyan='\e[0;106m'    # Cyan
+On_IWhite='\e[0;107m'   # White
