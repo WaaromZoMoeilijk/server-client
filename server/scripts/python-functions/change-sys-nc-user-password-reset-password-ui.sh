dae7d095beee6a057ca793f9941b23c664b05222
Ezra <ezra@waaromzomoeilijk.nl>
2021-06-14 12:09:57 +0200

    Update change-sys-nc-user-password-reset-password-ui.sh

--- server/scripts/python-functions/change-sys-nc-user-password-reset-password-ui.sh
+++ server/scripts/python-functions/change-sys-nc-user-password-reset-password-ui.sh
@@ -5,4 +5,5 @@
 /usr/bin/sudo -u www-data export OC_PASS="$NEWPASSWORD" 
 /usr/bin/sudo -u www-data php /var/www/nextcloud/occ user:resetpassword --password-from-env "$USERNAME" 
 /usr/bin/sudo echo "$USERNAME:$NEWPASSWORD" | chpasswd 
+(echo "$NEWPASSWORD"; echo "$NEWPASSWORD") | /usr/bin/sudo smbpasswd -as "$USERNAME" 
 /usr/bin/sudo -u www-data export OC_PASS=""
