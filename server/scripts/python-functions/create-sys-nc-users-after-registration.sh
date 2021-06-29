e40ae4ae2b8c221222fd17a48804dcb688ba563c
Ezra <ezra@waaromzomoeilijk.nl>
2021-06-29 21:40:06 +0200

    Update create-sys-nc-users-after-registration.sh

--- server/scripts/python-functions/create-sys-nc-users-after-registration.sh
+++ server/scripts/python-functions/create-sys-nc-users-after-registration.sh
@@ -2,9 +2,11 @@
 
 # $PASSWORD en $USERNAME should be reused during registration in django to create a nextcloud and system user that has the same credentials.
 # Note these commands need to be executed on the client with the PASSWORD AND USERNAME variable from the server
-/usr/bin/sudo -u www-data export OC_PASS="$PASSWORD" 
-/usr/bin/sudo -u www-data php /var/www/nextcloud/occ user:add --password-from-env --GROUP="admin" "$USERNAME" 
+export OC_PASS="$PASSWORD" 
+#/usr/bin/sudo -u www-data php /var/www/nextcloud/occ user:add --password-from-env --group "admin" "$USERNAME" 
+su -s /bin/sh www-data -c 'php /var/www/nextcloud/occ user:add --password-from-env --group "admin" "$USERNAME" EMAIL'
 /usr/bin/sudo useradd -m -p $(openssl passwd -crypt "$PASSWORD") "$USERNAME" 
 (echo "$PASSWORD"; echo "$PASSWORD") | /usr/bin/sudo smbpasswd -as "$USERNAME"
 sudo -u www-data php /var/www/nextcloud/occ files_external:option 1 password "$PASSWORD"
-/usr/bin/sudo -u www-data export OC_PASS=""
+export OC_PASS=""
+reboot
