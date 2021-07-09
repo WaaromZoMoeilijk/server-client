44810ab718f81af839f18777bf03c22f6f1f8698
WaaromZoMoeilijk <ezra@waaromzomoeilijk.nl>
2021-07-09 23:13:58 +0200

    Update post-activation.sh

--- client/scripts/post-activation.sh
+++ client/scripts/post-activation.sh
@@ -19,16 +19,20 @@ root_check
 #(/usr/bin/echo "$PASSWORD"; /usr/bin/echo "$PASSWORD") | /usr/bin/su -s /bin/sh www-data -c "/usr/bin/php /var/www/nextcloud/occ user:resetpassword $USERNAME"
 
 # Enable NC user
-/usr/bin/su -s /bin/sh www-data -c "/usr/bin/php /var/www/nextcloud/occ user:enable $USERNAME"
+#/usr/bin/su -s /bin/sh www-data -c "/usr/bin/php /var/www/nextcloud/occ user:enable $USERNAME"
 
 # Activate user
-curl --user "$USERNAME":"$PASSWORD" "http://127.0.0.1/nextcloud/login?clear=1"
+#curl -vv --user "$USERNAME":"$PASSWORD" "http://127.0.0.1/nextcloud/login?clear=1"
+#echo "Curl login"
 
 # Create PAM user
 /usr/bin/sudo useradd -m -p $(openssl passwd -crypt "$PASSWORD") "$USERNAME"  
 
 # Create SMB user
 (/usr/bin/echo "$PASSWORD"; /usr/bin/echo "$PASSWORD") | /usr/bin/sudo smbpasswd -as "$USERNAME" 
+
+# Samba
+usermod -aG www-data "$USERNAME"
 
 # Enable external files app
 /usr/bin/sudo -u www-data php /var/www/nextcloud/occ app:enable files_external
@@ -37,17 +41,17 @@ curl --user "$USERNAME":"$PASSWORD" "http://127.0.0.1/nextcloud/login?clear=1"
 if [ -f "/etc/samba/smb.conf" ]; then
 cat >> /etc/samba/smb.conf <<EOF
 [$USERNAME]
-valid users = $USERNAME
-Path = /mnt/dietpi_userdata/$USERNAME
+valid users = @www-data
+Path = /mnt/dietpi_userdata/nextcloud_data/$USERNAME/files
 Browseable = yes
 Writeable = Yes
 Guest ok = no
 Public = no
-#force group = www-data
-#create mask = 0770
-#directory mask = 0771
-#force create mode = 0660
-#force directory mode = 0770
+force group = www-data
+create mask = 0770
+directory mask = 0771
+force create mode = 0660
+force directory mode = 0770
 EOF
 else  
 echo "smb.conf doesn't exists, is it installed?"
@@ -55,47 +59,60 @@ apt install -y samba samba-common-bin smbclient
 	
 cat >> /etc/samba/smb.conf <<EOF
 [$USERNAME]
-valid users = $USERNAME
-Path = /mnt/dietpi_userdata/$USERNAME
+valid users = @www-data
+Path = /mnt/dietpi_userdata/nextcloud_data/$USERNAME/files
 Browseable = yes
 Writeable = Yes
 Guest ok = no
 Public = no
-#force group = www-data
-#create mask = 0770
-#directory mask = 0771
-#force create mode = 0660
-#force directory mode = 0770
+force group = www-data
+create mask = 0770
+directory mask = 0771
+force create mode = 0660
+force directory mode = 0770
 EOF
 fi
+echo "Samba config set"
 
 # Create dirs
 mkdir -p /var/log/samba 
-mkdir -p /mnt/dietpi_userdata/"$USERNAME"
+#mkdir -p /mnt/dietpi_userdata/"$USERNAME"
+echo "Created Dirs"
 
 # Permissions
 chown "$USERNAME":"$USERNAME" /mnt/dietpi_userdata/"$USERNAME"
+chmod 775 /mnt/dietpi_userdata/nextcloud_data/"$USERNAME"/files
+echo "Set permission"
 
 # Restart smbd
 service smbd restart && sleep 10
+echo "SMBD restarted"
 
 # Create external user share
-/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:create --user $USERNAME / smb password::password" | awk '{print $5}' > /home/dietpi/.smbid."$USERNAME"
-
+#/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:create --user $USERNAME / smb password::password" | awk '{print $5}' > /home/dietpi/.smbid."$USERNAME"
+#echo "Created external user share"
 # Set config
-SMBID=$(cat /home/dietpi/.smbid."$USERNAME")
-/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID host 127.0.0.1"
-/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID share $USERNAME"
-/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID username $USERNAME"
-/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID password $PASSWORD"
-/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID enable_sharing true"
+#SMBID=$(cat /home/dietpi/.smbid."$USERNAME")
+#echo "id"
+#/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID host 127.0.0.1"
+#echo "host"
+#/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID share $USERNAME"
+#echo "share"
+#/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID username $USERNAME"
+#echo "user"
+#/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID password $PASSWORD"
+#echo "pass"
+#/usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ files_external:config $SMBID enable_sharing true"
+#echo "sharing"
 
 # Delete admin
 /usr/bin/su -s /bin/sh www-data -c "php /var/www/nextcloud/occ user:delete admin"
+echo "Default admin removed"
 
 # cronjob to check for files smb vs nc
-crontab -l | { cat; echo "2 0 0 0 sudo -u www-data php /var/www/nextcloud/occ files:scan --all"; } | crontab -
+crontab -l | { cat; echo "*/10 0 0 0 0 pgrep "php" || sudo -u www-data php /var/www/nextcloud/occ files:scan --all"; } | crontab -
 crontab -l | { cat; echo "@reboot sudo -u www-data php /var/www/nextcloud/occ files_external:notify 1"; } | crontab -
+echo "Crontab"
 
 # install complete
 touch /home/dietpi/.smb_success
