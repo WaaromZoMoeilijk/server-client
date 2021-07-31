618eae7398bff54ab391929d6bca772d004af503
WaaromZoMoeilijk <ezra@waaromzomoeilijk.nl>
2021-07-31 15:42:20 +0200

    Update install.sh

--- server/install.sh
+++ server/install.sh
@@ -8,5 +8,23 @@ source <(curl -sL https://raw.githubusercontent.com/ezraholm50/server-client/mai
 apt install python3-virtualenv
 
 
+nginx
+server {
+       listen 2020;
+
+       server_name _;
+
+         location /static/ {
+                root /home/dietpi/pidjango;
+               # add_header Cache-Control "public, max-age=86400" always;
+        }
+       location / {
+                include proxy_params;
+                proxy_pass http://unix:/home/dietpi/pidjango/gunicorn.sock;
+        }
+}
+
+
+python3 server.py
 
 exit 0
