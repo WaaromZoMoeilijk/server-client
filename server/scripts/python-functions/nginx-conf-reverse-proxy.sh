3d2eae9cd3927a95a08ff3610dbd63215ce3698b
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-10 21:01:19 +0200

    Update nginx-conf-reverse-proxy.sh

--- server/scripts/python-functions/nginx-conf-reverse-proxy.sh
+++ server/scripts/python-functions/nginx-conf-reverse-proxy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Check for new clients, added to /var/www/pi/rpi_info/client-id.json
-# If new file, create new reverse proxy config for nextcloud.
+
 inotifywait -m /var/www/pi/rpi_info -e create -e moved_to |
 while read dir action file; do
 	echo "The file '$file' appeared in directory '$dir' via '$action'"
 
+	DATE=$(date)
+	PUBKEY=$(cat "$dir/$file" | jq -r .pubkey)
         USERNAME=$(cat "$dir/$file" | jq -r .userid)
 	NGINXCONF=$(echo "$file" | sed 's|.json|.conf|g')
         SSHPORT=$(cat "$dir/$file" | jq -r .ssh_port)
@@ -20,8 +21,11 @@ while read dir action file; do
 	# Check config
 	nginx -t || echo "Failed to set config please manually check" ; exit 1
 
+	# Set pubkey
+	echo "$file added on $DATE" >> /home/remote/.ssh/authorized_keys
+	echo "$PUBKEY" >> /home/remote/.ssh/authorized_keys
+	echo
+
 	# Reload new config
 	service nginx reload
 done
-
-exit 0
