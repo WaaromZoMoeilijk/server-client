877f46b4810d9d2375bb048528fb59eebab7378c
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-10 21:14:44 +0200

    Update tunnel_check.sh

--- client/scripts/tunnel_check.sh
+++ client/scripts/tunnel_check.sh
@@ -5,6 +5,7 @@ if n=$(ss | grep $(host wzc.waaromzomoeilijk.nl | awk {'print $4'}) | grep 9212)
 	exit 0
 else
 	echo "No reverse SSH tunnels found, connecting"
+	ssh -i /home/dietpi/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR 8022:localhost:22 remote@wzc.waaromzomoeilijk.nl -p 9212
 	ssh -i /home/dietpi/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/dietpi/ssh_port"):localhost:80 remote@wzc.waaromzomoeilijk.nl -p 9212 \
 	&& exit 0
 	echo "Failed to setup tunnel"
