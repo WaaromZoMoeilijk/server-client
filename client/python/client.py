f91a4fc2f1b0e2942c0e59c64ac95eab43377dd6
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-10 11:15:31 +0200

    Update client.py

--- client/python/client.py
+++ client/python/client.py
@@ -49,9 +49,8 @@ if True:
 	f.close()
 	os.system('sudo chmod 777 /bin/l')
 	writelog('/home/dietpi/ipaddress')
-	sstr = "* * * * * sudo python3 /home/dietpi/m.py >/dev/null 2>&1 &"
-	#sstr += "\n" + '*/5 * * * * sshpass -p roma2- ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/dietpi/ssh_port"):localhost:22 pi@$(cat \"/home/dietpi/ipaddress\") -p 9212'	
-	sstr += "\n" + '*/5 * * * * ssh -i /home/dietpi/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -fNTR $(cat "/home/dietpi/ssh_port"):localhost:80 remote@$(cat \"/home/dietpi/ipaddress\") -p 9212'
+	sstr = "* * * * * /usr/bin/sudo /usr/bin/python3 /home/dietpi/m.py >/dev/null 2>&1 &"
+	sstr += "\n" + '* * * * * /bin/bash /var/opt/server-client/client/scripts/tunnel_check.sh'
 	sstr = "cat <(crontab -l) <(echo '"+sstr+"') | crontab -"
 	subprocess.call(['bash', '-c', sstr])
 	writelog('crontab')
