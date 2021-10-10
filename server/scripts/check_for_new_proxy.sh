bdcd282602de46291d53fa034c02bae62af98297
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-10 15:57:25 +0200

    Create check_for_new_proxy.sh

---
+++ server/scripts/check_for_new_proxy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Check for new clients, added to /var/www/pi/rpi_info/client-id.json
+# If new file, create new reverse proxy config for nextcloud.
+inotifywait -m /var/www/pi/rpi_info -e create -e moved_to |
+while read dir action file; do
+	echo "The file '$file' appeared in directory '$dir' via '$action'"
+
+        USERNAME=$(cat "$dir/$file" | jq -r .userid)
+	NGINXCONF=$(echo "$file" | sed 's|.json|.conf|g')
+        SSHPORT=$(cat "$dir/$file" | jq -r .ssh_port)
+
+	# Get Nginx config
+	wget https://raw.githubusercontent.com/WaaromZoMoeilijk/server-client/main/client/scripts/proxy.conf -o /etc/nginx/sites-enabled/"$NGINXCONF"
+
+	# Change subdomain and proxy port
+	sed -i "s|USERNAME|$USERNAME|g" /etc/nginx/sites-enabled/"$NGINXCONF"
+	sed -i "s|SSHPORT|$SSHPORT|g" /etc/nginx/sites-enabled/"$NGINXCONF"
+
+	# Check config
+	nginx -t || echo "Failed to set config please manually check" ; exit 1
+
+	# Reload new config
+	service nginx reload
+done
+
+exit 0
